459efd607dd8c26c0f1d99a75de680cad90cfb25
Don Cooper <don_cooper@hotmail.com>
2017-06-15 15:12:09 -0400

    Reservations controller set up

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,43 @@
 class ReservationsController < ApplicationController
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @reservation = @restaurant.reservations.new(reservation_params)
+
+    if @reservation.save
+      flash[:alert] = "The reservation has been saved."
+      render :show
+    else
+      render 'restaurants/show'
+    end
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def update
+  @restaurant = Restaurant.find(params[:restaurant_id])
+  @reservation = Reservation.find(params[:id])
+    if @reservation.update(reservation_params)
+      flash[:alert] = "The reservation has been updated"
+      render :show
+    else
+      render 'restaurants/show'
+    end
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @reservation = Reservation.find(params[:id])
+    @reservation.destroy
+    flash[:alert] = "The reservation has been deleted"
+    render 'restaurants/show'
+  end
+
+  def reservation_params
+		params.require(:reservation).permit(:date, :time, :party_size)
+	end
+
 end
