07a10d77ae9f057e04def9d67908d6627c341096
Don Cooper <don_cooper@hotmail.com>
2017-06-15 15:19:54 -0400

    Users controller set up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,53 @@
 class UsersController < ApplicationController
+
+  def index
+		@user = User.all
+	end
+
+  def new
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save
+			flash[:alert] = "The user has been saved"
+			render :show
+		else
+			render :new
+		end
+	end
+
+	def show
+		@user = User.find(params[:id])
+    @reservation = Reservation.new
+		render :show
+	end
+
+  def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			flash[:alert] = "The user has been updated"
+			redirect_to @user
+		else
+			redirect_back_or_to @user
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		flash[:alert] = "The user has been deleted"
+		redirect_to users_path
+	end
+
+	def user_params
+		params.require(:user).permit(:name, :email, :phone)
+	end
+
 end
