0d7f7bf95eb922b0a07e83b778be43315859403a
Don Cooper <don_cooper@hotmail.com>
2017-06-15 14:59:50 -0400

    Models set up

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,6 @@
 class Restaurant < ApplicationRecord
+
+  has_many :reservations
+  has_many :users, through: :reservations
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+
+  has_many :reservations
+  has_many :restaurants, through: :reservations 
+
 end
