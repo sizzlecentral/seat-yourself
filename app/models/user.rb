c7e70c7e75fd74ae6f50c8985cbb808ea349c110
Don Cooper <don_cooper@hotmail.com>
2017-06-16 15:22:49 -0400

    Changes to user model, application controller, gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
 
-  # has_many :reservations
-  # has_many :restaurants, through: :reservations
+  has_many :reservations
+  has_many :restaurants, through: :reservations
+
+  has_secure_password
+
+  validates :name, :email, :phone, presence: true
 
 end
