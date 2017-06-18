2c74870073e34929d7e27eeb879db9e93bb5c275
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-18 16:18:13 -0400

    Update restaurant create method include owner_id, create new restaurant form - unsure if works relies on session id to add owner_id

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,53 +1,56 @@
 class RestaurantsController < ApplicationController
 
-	def index
-		@restaurants = Restaurant.all
-	end
+  def index
+    @restaurants = Restaurant.all
+  end
 
-	def new
-		@restaurant = Restaurant.new
-	end
+  def new
+    @restaurant = Restaurant.new
+  end
 
-	def create
-		@restaurant = Restaurant.new(restaurant_params)
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    # NOT sure if this is right???
+    @restaurant.owner_id = session[:user_id]
 
-		if @restaurant.save
-			flash[:alert] = "The restaurant has been saved"
-			render :show
-		else
-			render :new
-		end
-	end
+    if @restaurant.save
+      flash[:alert] = "The restaurant has been saved"
+      # Why do we render instead of redirect to show?
+      render :show
+    else
+      render :new
+    end
+  end
 
-	def show
-		@restaurant = Restaurant.find(params[:id])
-		@reservation = Reservation.new
-		render :show
-	end
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    @reservation = Reservation.new
+    render :show
+  end
 
-	def edit
-		@restaurant = Restaurant.find(params[:id])
-	end
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
 
-	def update
-		@restaurant = Restaurant.find(params[:id])
-		if @restaurant.update(restaurant_params)
-			flash[:alert] = "The restaurant has been updated"
-			redirect_to @restaurant
-		else
-			redirect_back_or_to @restaurant
-		end
-	end
+  def update
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_params)
+      flash[:alert] = "The restaurant has been updated"
+      redirect_to @restaurant
+    else
+      redirect_back_or_to @restaurant
+    end
+  end
 
-	def destroy
-		@restaurant = Restaurant.find(params[:id])
-		@restaurant.destroy
-		flash[:alert] = "The restaurant has been deleted"
-		redirect_to restaurants_path
-	end
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:alert] = "The restaurant has been deleted"
+    redirect_to restaurants_path
+  end
 
-	def restaurant_params
-		params.require(:restaurant).permit(:name, :address, :price_range, :summary, :neighbourhood, :website, :menu_url, :capacity, :opens_at, :closes_at)
-	end
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :price_range, :summary, :neighbourhood, :website, :menu_url, :capacity, :opens_at, :closes_at)
+  end
 
 end
