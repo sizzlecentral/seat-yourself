a563f8a84090c86768eb7ac35dbcb69e16ed16a2
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-19 12:56:26 -0400

    Revert to 'owner_id' as foreign key in restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
 
-  belongs_to  :owner, class_name: "User", optional: true
+  belongs_to :owner, class_name: "User", optional: true
   has_many :reservations
   has_many :users, through: :reservations
 
