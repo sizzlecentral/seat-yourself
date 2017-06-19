384c397a2a2e5ba901d24f16c6977aa9b482ff08
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-18 20:21:10 -0400

    Updated my last migration to remove owner_id

--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 20170619000056) do
     t.integer "closes_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "owner_id"
     t.integer "user_id"
   end
 
