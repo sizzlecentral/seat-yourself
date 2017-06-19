d038cad77c8c34bca0a74a55931d04965c06811f
Don Cooper <don_cooper@hotmail.com>
2017-06-19 13:32:42 -0400

    Resolving conflicts

--- db/migrate/20170619000056_change_restaurant_table.rb
+++ db/migrate/20170619000056_change_restaurant_table.rb
@@ -1,6 +1,4 @@
 class ChangeRestaurantTable < ActiveRecord::Migration[5.1]
   def change
-    add_column :restaurants, :user_id, :integer
-    remove_column :restaurants, :owner_id, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170617200137) do
+ActiveRecord::Schema.define(version: 20170619165407) do
 
   create_table "reservations", force: :cascade do |t|
     t.date "date"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20170617200137) do
     t.integer "closes_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.integer "owner_id"
   end
 
   create_table "sessions", force: :cascade do |t|
