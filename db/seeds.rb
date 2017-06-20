6a5815845698d382def95b72986505da847ef5d5
Vlad Wolanyk <vladwolanyk@yahoo.ca>
2017-06-20 14:35:14 -0400

    Modify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,42 +4,46 @@ Reservation.destroy_all
 
 
 parts = Restaurant.create!(
-	name:           'Parts & Labour',
-	address:        '123 Queen St. W.',
-	price_range:     2,
-	summary:        'Offal',
-	neighbourhood:  'Parkdale',
-	website:        'partsandlabour.com',
-	menu_url:       'partsandlabour.com/menu',
-	capacity:       80,
-	opens_at:       6,
-	closes_at:      11
+  name:           'Parts & Labour',
+  address:        '123 Queen St. W.',
+  price_range:     2,
+  summary:        'Offal',
+  neighbourhood:  'Parkdale',
+  website:        'partsandlabour.com',
+  menu_url:       'partsandlabour.com/menu',
+  capacity:       80,
+  opens_at:       6,
+  closes_at:      11,
+  owner_id:       2
+
 )
 
 canoe = Restaurant.create!(
-	name:           'Canoe',
-	address:        '5 Yonge St.',
-	price_range:     5,
-	summary:        'Fancy pants',
-	neighbourhood:  'Financial district',
-	website:        'canoe.com',
-	menu_url:       'canoe.com/menu',
-	capacity:       80,
-	opens_at:       7,
-	closes_at:      10
+  name:           'Canoe',
+  address:        '5 Yonge St.',
+  price_range:     5,
+  summary:        'Fancy pants',
+  neighbourhood:  'Financial district',
+  website:        'canoe.com',
+  menu_url:       'canoe.com/menu',
+  capacity:       80,
+  opens_at:       7,
+  closes_at:      10
+  owner_id:       2
 )
 
 subway = Restaurant.create!(
-	name:           'Subway',
-	address:        '987 King St. W.',
-	price_range:     1,
-	summary:        'Made your way',
-	neighbourhood:  'Entertainment district',
-	website:        'subway.com',
-	menu_url:       'subway.com/menu',
-	capacity:       12,
-	opens_at:       6,
-	closes_at:      12
+  name:           'Subway',
+  address:        '987 King St. W.',
+  price_range:     1,
+  summary:        'Made your way',
+  neighbourhood:  'Entertainment district',
+  website:        'subway.com',
+  menu_url:       'subway.com/menu',
+  capacity:       12,
+  opens_at:       6,
+  closes_at:      12,
+  owner_id:       3
 )
 
 bob = User.create!(
