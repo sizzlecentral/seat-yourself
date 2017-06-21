1fe5713850d0bdb2a34377d09e4417d4380928a2
Ju-Hang Sin <juhangsin@gmail.com>
2017-06-21 09:56:08 -0400

    automatic login fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,51 +1,52 @@
 class UsersController < ApplicationController
 
-  def index
-    @user = User.all
-  end
+	def index
+		@user = User.all
+	end
 
-  def new
-    @user = User.new
-  end
+	def new
+		@user = User.new
+	end
 
-  def create
-    @user = User.new(user_params)
+	def create
+		@user = User.new(user_params)
 
-    if @user.save
-      flash[:notice] = 'The user has been saved'
-      redirect_to restaurants_path
-    else
-      render :new
-    end
-  end
+		if @user.save
+			session[:user_id] = @user.id
+			flash[:notice] = 'The user has been saved'
+			redirect_to restaurants_path
+		else
+			render :new
+		end
+	end
 
-  def show
-    @user = current_user
-  end
+	def show
+		@user = current_user
+	end
 
-  def edit
-    @user = current_user
-  end
+	def edit
+		@user = current_user
+	end
 
-  def update
-    @user = current_user
-    if @user.update(user_params)
-      flash[:alert] = "The user has been updated"
-      redirect_to users_path(@user)
-    else
-      redirect_back_or_to users_path(@user)
-    end
-  end
+	def update
+		@user = current_user
+		if @user.update(user_params)
+			flash[:alert] = "The user has been updated"
+			redirect_to users_path(@user)
+		else
+			redirect_back_or_to users_path(@user)
+		end
+	end
 
-  def destroy
-    @user = current_user
-    @user.destroy
-    flash[:alert] = "The user has been deleted"
-    redirect_to users_path
-  end
+	def destroy
+		@user = current_user
+		@user.destroy
+		flash[:alert] = "The user has been deleted"
+		redirect_to users_path
+	end
 
-  def user_params
-    params.require(:user).permit(:name, :email, :phone, :role, :password, :password_confirmation)
-  end
+	def user_params
+		params.require(:user).permit(:name, :email, :phone, :role, :password, :password_confirmation)
+	end
 
 end
